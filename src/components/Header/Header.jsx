885a6eb8f1f4ac392bcb914d4f0861917a6030ba
byexend <danilka.balash.95@gmail.com>
2022-10-18 23:24:41 +0500

    add style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
-import {Container} from './HeaderStyled'
+import {Logo, Riot} from "../../assets/svg/SvgImporter"
+import {Container, NavLogo, Dash} from './HeaderStyled'
 const Header = () => {
+    const NavEl = ["Game", "Champions", "News", "Patch Notes", "Discover", "Esports", "Universe", "Shop", "Support"]
     return(
-        <Container></Container>
+        <Container>
+            <NavLogo>
+            <Riot/>
+            <Dash/>
+            <Logo/>
+            </NavLogo>
+        </Container>
     )
 }
 export default Header
--- src/components/Header/HeaderStyled.jsx
+++ src/components/Header/HeaderStyled.jsx
@@ -3,5 +3,15 @@ import styled from "styled-components"
 export const Container = styled.div`
     width: 100%;
     height: 100%;
-    background-color: #000;
+    background-color: #1F2326;
+`
+export const NavLogo = styled.div`
+    display: flex;
+    width: 250px;
+    justify-content: space-between;
+`
+export const Dash = styled.div`
+    width: 2px;
+    height: 42px;
+    background-color: #fff;
 `
