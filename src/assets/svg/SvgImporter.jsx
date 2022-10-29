01320afa88a59b73c944f206c4d464012a34a271
byexend <danilka.balash.95@gmail.com>
2022-10-29 23:13:35 +0500

    add svg

--- src/assets/svg/SvgImporter.jsx
+++ src/assets/svg/SvgImporter.jsx
@@ -1,4 +1,23 @@
 export {ReactComponent as Logo} from "./logo.svg"
 export {ReactComponent as Riot} from "./riot_games.svg"
 export {ReactComponent as Arrow} from "./Arrow.svg"
-export {ReactComponent as Jett_Abilities} from "./Jett/Jett_Abilities0.svg"
+
+export {ReactComponent as Phoenix_Abilities0} from "./Phoenix/Phoenix_Abilities0.svg"
+export {ReactComponent as Phoenix_Abilities1} from "./Phoenix/Phoenix_Abilities1.svg"
+export {ReactComponent as Phoenix_Abilities2} from "./Phoenix/Phoenix_Abilities2.svg"
+export {ReactComponent as Phoenix_Abilities3} from "./Phoenix/Phoenix_Abilities3.svg"
+
+export {ReactComponent as Jett_Abilities0} from "./Jett/Jett_Abilities0.svg"
+export {ReactComponent as Jett_Abilities1} from "./Jett/Jett_Abilities1.svg"
+export {ReactComponent as Jett_Abilities2} from "./Jett/Jett_Abilities2.svg"
+export {ReactComponent as Jett_Abilities3} from "./Jett/Jett_Abilities3.svg"
+
+export {ReactComponent as Sova_Abilities0} from "./Sova/Sova_Abilities0.svg"
+export {ReactComponent as Sova_Abilities1} from "./Sova/Sova_Abilities1.svg"
+export {ReactComponent as Sova_Abilities2} from "./Sova/Sova_Abilities2.svg"
+export {ReactComponent as Sova_Abilities3} from "./Sova/Sova_Abilities3.svg"
+
+export {ReactComponent as Sage_Abilities0} from "./Sage/Sage_Abilities0.svg"
+export {ReactComponent as Sage_Abilities1} from "./Sage/Sage_Abilities1.svg"
+export {ReactComponent as Sage_Abilities2} from "./Sage/Sage_Abilities2.svg"
+export {ReactComponent as Sage_Abilities3} from "./Sage/Sage_Abilities3.svg"
